b78439b9c73fd16478ee62c01f590b37e5d17a48
bete <beteme390@gmail.com>
2024-05-10 21:08:29 +0300

    cart

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
-function Navbar({setShowLogin}) {
+function Navbar({ setShowLogin }) {
   const [menu, setMenu] = useState("home");
 
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="" className="logo" />
+      <Link to="/">
+        <img src={assets.logo} alt="" className="logo" />
+      </Link>
       <ul className="navbar-menu">
         <Link
           to="/"
@@ -41,7 +43,9 @@ function Navbar({setShowLogin}) {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="" />
+          </Link>
           <div className="dot"></div>
         </div>
         <button onClick={() => setShowLogin(true)}>sign in</button>
--- frontend/src/Pages/Cart/Cart.jsx
+++ frontend/src/Pages/Cart/Cart.jsx
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { useContext } from "react";
+import { StoreContext } from "../../Context/StoreContext";
+import "./Cart.css";
 
 function Cart() {
-  return <div></div>;
-}
+  const { cartItems, food_list, removeFromCart,getTotalCartAmount } = useContext(StoreContext);
 
+  return (
+    <div className="cart">
+      <div className="cart-item">
+        <div className="cart-item-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div>
+                <div className="cart-item-title  cart-items-item" key={index}>
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>${item.price * cartItems[item._id]}</p>
+                  <p onClick={() => removeFromCart(item._id)} className="cross">
+                    x
+                  </p>
+                </div>
+                <hr />
+              </div>
+            );
+          }
+          return null; // Add a return statement for the else case
+        })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+
+          <div className="cart-total-details">
+            <p>Subtotal</p>
+            <p>{getTotalCartAmount()}</p>
+          </div>
+          <hr />
+          <div className="cart-total-details">
+            <p>Delivery fee</p>
+            <p>{5}</p>
+          </div>
+          <hr />
+          <div className="cart-total-details">
+            <b>Total</b>
+            <b>{getTotalCartAmount()}</b>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cart-promocode">
+          <div>
+            <p>if you have a promo code, enter here</p>
+            <div className="cart-promocode-input">
+              <input type="text" placeholder="promocode" />
+              <button>submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default Cart;
